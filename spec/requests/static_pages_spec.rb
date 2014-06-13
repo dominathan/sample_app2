f7bdd3f08bd9ab6d748de08733b9c9b553d32560
Nathan Hall <nathan.mh@gmail.com>
2014-06-13 17:02:12 -0500

    finish static page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content("Sample App")
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Nathan | Home")
+    end
   end
 
   describe "Help page" do
@@ -22,6 +27,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Nathan | Help")
+    end
   end
 
   describe "About page" do
@@ -29,6 +39,11 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Nathan | About Us")
     end
   end
 
