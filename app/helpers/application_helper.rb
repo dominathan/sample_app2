933ec1233dc664d6f105a2b05cc27867c12764ef
Nathan Hall <nathan.mh@gmail.com>
2014-06-13 17:23:57 -0500

    begin chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  #return full title on per-page basis
+
+  def full_title(page_title)
+    base_title = "Nathan"
+    if page_title.empty?
+      return base_title
+    else
+      return "#{base_title} | #{page_title}"
+    end
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,10 +15,16 @@ describe "StaticPages" do
       expect(page).to have_content("Sample App")
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Nathan | Home")
+      expect(page).to have_title("Nathan")
     end
+
+    it "should have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title (' |Home')
+    end
+
   end
 
   describe "Help page" do
