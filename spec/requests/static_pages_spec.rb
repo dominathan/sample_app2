fd93b1c997fee7ebc62e9c106607e460ba4baf6d
Nathan Hall <nathan.mh@gmail.com>
2014-06-13 19:41:39 -0500

    added a bunch of bootstrap and sass shit.  working on routes now

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,17 +11,17 @@ describe "StaticPages" do
 
   describe "Home page" do
     it 'should have the content "Sample App"' do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content("Sample App")
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Nathan")
     end
 
     it "should have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title (' |Home')
     end
 
@@ -30,12 +30,12 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Nathan | Help")
     end
   end
@@ -43,13 +43,26 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Nathan | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Nathan | Contact")
     end
   end
 
